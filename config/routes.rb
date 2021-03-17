1a58f26e5e950218b05f7827c04ecfb2993ccf84
masahiro-tanaka0716 <s1377062@gmail.com>
2021-03-17 05:26:11 +0000

    favorite iine

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#top"
   resource :user, except: [:new, :create, :destroy]
-  resources :recipes
+  resources :recipes do 
+    resource :favorites, only: [:create, :destroy]
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_065944) do
+ActiveRecord::Schema.define(version: 2021_03_17_043407) do
+
+  create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
